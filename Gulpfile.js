b87019300446a532b56819d67f577f90f5f151d1
benatespina <benatespina@gmail.com>
2017-02-15 08:56:46 +0100

    Simplified the usage of the grid, generate the standalone css distribution and other minor improvements

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,22 +1,29 @@
-// This file is part of the LIN3S CSS Grid library.
-//
-// Copyright (c) 2016-2017 LIN3S <info@lin3s.com>
-//
-// For the full copyright and license information, please view the LICENSE
-// file that was distributed with this source code.
-//
-// @author Gorka Laucirica <gorka.lauzirika@gmail.com>
-// @author Beñat Espiña <benatespina@gmail.com>
+/*
+ * This file is part of the LIN3S CSS Grid library.
+ *
+ * Copyright (c) 2016-present LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Gorka Laucirica <gorka.lauzirika@gmail.com>
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
 
 var gulp = require('gulp');
+var fs = require('fs');
+var cssNano = require('gulp-cssnano');
+var license = require('gulp-header-license');
 var sass = require('gulp-sass');
 var scsslint = require('gulp-scss-lint');
+var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
   sass: './scss',
   exampleCss: './example/css',
-  exampleSass: './example/scss'
+  exampleSass: './example/scss',
+  dist: './dist'
 };
 
 gulp.task('scss-lint', function () {
@@ -24,6 +31,26 @@ gulp.task('scss-lint', function () {
     .pipe(scsslint({
       'config': './.scss_lint.yml'
     }));
+});
+
+gulp.task('dist', function () {
+  var year = (new Date()).getFullYear();
+
+  gulp.src(paths.sass + '/dist.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssNano({
+      keepSpecialComments: 1,
+      rebase: false
+    }))
+    .pipe(rename({
+      basename: 'lin3s-css-grid',
+      suffix: '.min'
+    }))
+    .pipe(license(
+      fs.readFileSync('header.txt', 'utf8'),
+      {year: year}, 0.9)
+    )
+    .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('example-css', function () {
--- README.md
+++ README.md
@@ -21,50 +21,22 @@ $ bower install --save lin3s-css-grid
 
 After installation process, you can include css file in your html.
 ```html
-<link href="/your/path/lin3s-css-grid/dist/lin3scssgrid.min.css" type="text/css" rel="stylesheet">
+<link href="/your/path/lin3s-css-grid/dist/lin3s-css-grid.min.css" type="text/css" rel="stylesheet">
 ```
 However is highly recommend to use the Sass version of the grid including files as Sass's imports.
 The best practice is to create a row.scss component that wraps all the grid generation logic. Basic example can be the
 following code:
 
 ```scss
-@import '../../scss/functions';
-@import '../../scss/mixins';
-@import '../../scss/variables';
+$grid-row-type: 'adaptive';
+$grid-row-column-count: 12;
 
 $small-cols: 6;
 $medium-cols: 3, 4, 6;
 $large-cols: 1, 2, 3, 4, 6;
 $xlarge-cols: 6;
 
-@include grid__row('.row', 'adaptive');
-
-.row__column {
-  @include grid__column(12);
-}
-
-.row--centered {
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.row--collapse {
-  padding-left: $column-gutter / 2;
-  padding-right: $column-gutter / 2;
-
-  > .row__column {
-    padding-left: 0;
-    padding-right: 0;
-  }
-}
-
-@include grid-column-for-media-query('small', $small-cols, $small-up);
-@include grid-column-for-media-query('medium', $medium-cols, $medium-up);
-@include grid-column-for-media-query('large', $large-cols, $large-up);
-@include grid-column-for-media-query('xlarge', $xlarge-cols, $xlarge-up);
-
-@include grid-column-push-for-media-query('large', $large-cols, $large-up);
-@include grid-column-push-for-media-query('xlarge', $xlarge-cols, $xlarge-up);
+@import '../../node_modules/lin3s-css-grid/scss/row';
 ```
 
 ##Licensing Options
